6a5c53e9be9d60440429bcbe334fc52ab010a5fa
chntlg <chantalguerrero04@gmail.com>
2024-09-13 17:47:29 +0800

    Initial commit with strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Anne";
+let favActorLastName = "Hathaway";
+let fullName = favActorFirstName +' '+ favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let pet = "chico"
+let lowercase = pet.toUpperCase();
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+ message += " her best show is The Princess Diaries"
+
+console.log("First name :", favActorFirstName);
+console.log("Last Name :", favActorLastName);
+console.log("Full name :", fullName);
+console.log("Lowercase Full name :", uppercase);
+console.log("Pet name :", lowercase);
+console.log(message)
